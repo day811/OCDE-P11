1a9a5c7b06418f7d07b019bb521f95284fe7b37f
day811 <day811@laposte.net>
2026-02-19 15:51:01 +0100

    fix BASE_DIR after moving file

--- src/config.py
+++ src/config.py
@@ -29,7 +29,7 @@ class Config:
     """Manage snapshot versions and index selection"""
     
     # ============= PATHS =============
-    BASE_DIR = Path(__file__).parent
+    BASE_DIR = Path(__file__).parent.parent
     DATA_DIR = BASE_DIR / "data"
     RAW_DATA_DIR = DATA_DIR / "raw"
     PROCESSED_DATA_DIR = DATA_DIR / "processed"
